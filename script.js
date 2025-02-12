8b6592b800bf92b4af86f076953d85612db665f7
simfrisk <simoncarlfrisk@gmail.com>
2025-02-12 14:27:10 +0100

    New shop layout and images

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const joinBtn = document.getElementById("joinBtn");
+const shopBtn = document.getElementById("shopBtn");
+
 
 joinBtn.onclick = function () {
   window.location.href = "newMember.html";
 
+};
+
+shopBtn.onclick = function () {
+  window.location.href = "shop.html";
 };
