9a3781f432a261d9653f45d219cef76603d89698
simfrisk <simoncarlfrisk@gmail.com>
2025-02-16 20:12:19 +0100

    New Sturckture

--- script/newMember.js
+++ script/newMember.js
@@ -1,7 +1,24 @@
 const shopBtn = document.getElementById("shopBtn");
+const joinBtn = document.getElementById("joinBtn");
+const shopBtn2 = document.getElementById("shopBtn2");
+const burgerBtn = document.getElementById("burger-button");
+const burgerMenu = document.getElementById("burger-menu");
+
 
 joinBtn.onclick = function () {
   window.location.href = "newMember.html";
 
 };
 
+shopBtn.onclick = function () {
+  window.location.href = "html/shop.html";
+};
+
+shopBtn2.onclick = function () {
+  window.location.href = "html/shop.html";
+};
+
+burgerBtn.onclick = function () {
+  burgerMenu.classList.toggle("open");
+  burgerBtn.style.color = burgerBtn.style.color === "gray" ? "black" : "gray";
+};
